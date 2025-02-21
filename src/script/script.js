3dab3a0aa182994f424b1794b6e5dc426cc856c7
Masoud Moghaddam <zibadis@gmail.com>
2025-02-21 02:25:37 -0800

    some sourse file repair

--- src/script/script.js
+++ src/script/script.js
@@ -1,15 +1,15 @@
 
-    const ZibadisHoldingIcon = 'src/images/logos/zibadis-holding.png';
+    const ZibadisHoldingIcon = './src/images/logos/zibadis-holding.png';
 
     const plateIcons = [
-      'src/images/logos/3dEsite.png',
-      'src/images/logos/Antopeloni.png',
-      'src/images/logos/Zibadis.png',
-      'src/images/logos/ZibadisPlay.png',
-      'src/images/logos/ZinatNet.png',
-      'src/images/logos/AsaieJadoo.png',
-      'src/images/logos/sahfayaft.png',
-      'src/images/logos/yottabyte.png',
+      './src/images/logos/3dEsite.png',
+      './src/images/logos/Antopeloni.png',
+      './src/images/logos/Zibadis.png',
+      './src/images/logos/ZibadisPlay.png',
+      './src/images/logos/ZinatNet.png',
+      './src/images/logos/AsaieJadoo.png',
+      './src/images/logos/sahfayaft.png',
+      './src/images/logos/yottabyte.png',
     ];
  
 
@@ -20,81 +20,81 @@
       [
    
 
-        { label: "apps", link: "src/pages/3desite/apps.html" },
-        { label: "blog", link: "src/pages/3desite/blog.html" },
-        { label: "tutorial", link: "src/pages/3desite/tutorial.html" },
-        { label: "about", link: "src/pages/3desite/about.html" },
-        { label: "shop", link: "src/pages/3desite/shop.html" },
-        { label: "services", link: "src/pages/3desite/services.html" },
+        { label: "apps", link: "./src/pages/3desite/apps.html" },
+        { label: "blog", link: "./src/pages/3desite/blog.html" },
+        { label: "tutorial", link: "./src/pages/3desite/tutorial.html" },
+        { label: "about", link: "./src/pages/3desite/about.html" },
+        { label: "shop", link: "./src/pages/3desite/shop.html" },
+        { label: "services", link: "./src/pages/3desite/services.html" },
       
       ],
       //    2-Antopeloni
       [
      
-      { label: "apps", link: "src/pages/antopleoni/apps.html" },
-      { label: "blog", link: "src/pages/antopleoni/blog.html" },
-      { label: "tutorial", link: "src/pages/antopeloni/tutorial.html" },
-      { label: "about", link: "src/pages/antopeloni/about.html" },
-      { label: "shop", link: "src/pages/antopeloni/shop.html" },
-      { label: "services", link: "src/pages/antopeloni/services.html" },
+      { label: "apps", link: "./src/pages/antopleoni/apps.html" },
+      { label: "blog", link: "./src/pages/antopleoni/blog.html" },
+      { label: "tutorial", link: "./src/pages/antopeloni/tutorial.html" },
+      { label: "about", link: "./src/pages/antopeloni/about.html" },
+      { label: "shop", link: "./src/pages/antopeloni/shop.html" },
+      { label: "services", link: "./src/pages/antopeloni/services.html" },
       ],
       //  3-Zibadis
       [ 
-         { label: "apps", link: "src/pages/zibadis/apps.html" },
-         { label: "blog", link: "src/pages/zibadis/blog.html" },
-         { label: "tutorial", link: "src/pages/zibadistutorial.html" },
-         { label: "about", link: "src/pages/zibadis/about.html" },
-         { label: "shop", link: "src/pages/zibadis/shop.html" },
-         { label: "services", link: "src/pages/zibadis/services.html" },
+         { label: "apps", link: "./src/pages/zibadis/apps.html" },
+         { label: "blog", link: "./src/pages/zibadis/blog.html" },
+         { label: "tutorial", link: "./src/pages/zibadistutorial.html" },
+         { label: "about", link: "./src/pages/zibadis/about.html" },
+         { label: "shop", link: "./src/pages/zibadis/shop.html" },
+         { label: "services", link: "./src/pages/zibadis/services.html" },
      
       ],
       //  4-zibadisplay
       [ 
-        { label: "apps", link: "src/pages/zibadisplay/apps.html" },
-        { label: "blog", link: "src/pages/zibadisplay/blog.html" },
-        { label: "tutorial", link: "src/pages/zibadisplay/tutorial.html" },
-        { label: "about", link: "src/pages/zibadisplay/about.html" },
-        { label: "shop", link: "src/pages/zibadisplay/shop.html" },
-        { label: "services", link: "src/pages/zibadisplay/services.html" },
+        { label: "apps", link: "./src/pages/zibadisplay/apps.html" },
+        { label: "blog", link: "./src/pages/zibadisplay/blog.html" },
+        { label: "tutorial", link: "./src/pages/zibadisplay/tutorial.html" },
+        { label: "about", link: "./src/pages/zibadisplay/about.html" },
+        { label: "shop", link: "./src/pages/zibadisplay/shop.html" },
+        { label: "services", link: "./src/pages/zibadisplay/services.html" },
       ],
       //5-zinat.net
       [
      
-      { label: "apps", link: "src/pages/zinatnet/apps.html" },
-      { label: "blog", link: "src/pages/zinatnet/blog.html" },
-      { label: "tutorial", link: "src/pages/zinatnet/tutorial.html" },
-      { label: "about", link: "src/pages/zinatnet/about.html" },
-      { label: "shop", link: "src/pages/zinatnet/shop.html" },
-      { label: "services", link: "src/pages/zinatnet/services.html" },
+      { label: "apps", link: "./src/pages/zinatnet/apps.html" },
+      { label: "blog", link: "./src/pages/zinatnet/blog.html" },
+      { label: "tutorial", link: "./src/pages/zinatnet/tutorial.html" },
+      { label: "about", link: "./src/pages/zinatnet/about.html" },
+      { label: "shop", link: "./src/pages/zinatnet/shop.html" },
+      { label: "services", link: "./src/pages/zinatnet/services.html" },
       ],
       // 6-Asaiejadoo 
       [
      
-      { label: "apps", link: "src/pages/asaiejadoo/apps.html" },
-      { label: "blog", link: "src/pages/asaiejadoo/blog.html" },
-      { label: "tutorial", link: "src/pages/asaiejadoo/tutorial.html" },
-      { label: "about", link: "src/pages/asaiejadoo/about.html" },
-      { label: "shop", link: "src/pages/asaiejadoo/shop.html" },
-      { label: "services", link: "src/pages/asaiejadoo/services.html" },
+      { label: "apps", link: "./src/pages/asaiejadoo/apps.html" },
+      { label: "blog", link: "./src/pages/asaiejadoo/blog.html" },
+      { label: "tutorial", link: "./src/pages/asaiejadoo/tutorial.html" },
+      { label: "about", link: "./src/pages/asaiejadoo/about.html" },
+      { label: "shop", link: "./src/pages/asaiejadoo/shop.html" },
+      { label: "services", link: "./src/pages/asaiejadoo/services.html" },
       ],
       // 7-shafayaft
       [,
-        { label: "apps", link: "src/pages/shafayaft/apps.html" },
-        { label: "blog", link: "src/pages/shafayaft/blog.html" },
-        { label: "tutorial", link: "src/pages/shafayaft/tutorial.html" },
-        { label: "about", link: "src/pages/shafayaft/about.html" },
-        { label: "shop", link: "src/pages/shafayaft/shop.html" },
-        { label: "services", link: "src/pages/shafayaft/services.html" },
+        { label: "apps", link: "./src/pages/shafayaft/apps.html" },
+        { label: "blog", link: "./src/pages/shafayaft/blog.html" },
+        { label: "tutorial", link: "./src/pages/shafayaft/tutorial.html" },
+        { label: "about", link: "./src/pages/shafayaft/about.html" },
+        { label: "shop", link: "./src/pages/shafayaft/shop.html" },
+        { label: "services", link: "./src/pages/shafayaft/services.html" },
        
       ],
       //  8-yottabyte
       [,
-        { label: "apps", link: "src/pages/yottabyte/apps.html" },
-        { label: "blog", link: "src/pages/yottabyte/blog.html" },
-        { label: "tutorial", link: "src/pages/yottabyte/tutorial.html" },
-        { label: "about", link: "src/pages/yottabyte/about.html" },
-        { label: "shop", link: "src/pages/yottabyte/shop.html" },
-        { label: "services", link: "src/pages/yottabyte/services.html" },
+        { label: "apps", link: "./src/pages/yottabyte/apps.html" },
+        { label: "blog", link: "./src/pages/yottabyte/blog.html" },
+        { label: "tutorial", link: "./src/pages/yottabyte/tutorial.html" },
+        { label: "about", link: "./src/pages/yottabyte/about.html" },
+        { label: "shop", link: "./src/pages/yottabyte/shop.html" },
+        { label: "services", link: "./src/pages/yottabyte/services.html" },
         
       ],
     ];
@@ -182,18 +182,18 @@ scene.add(mainGroup);
     const textureLoader = new THREE.TextureLoader(loadingManager);
     const cubeTextureLoader = new THREE.CubeTextureLoader()
     const environmentMap = cubeTextureLoader.load([
-      '/src/images/environmentMap/px.png',
-      '/src/images/environmentMap/nx.png',
-      '/src/images/environmentMap/py.png',
-      '/src/images/environmentMap/ny.png',
-      '/src/images/environmentMap/pz.png',
-      '/src/images/environmentMap/nz.png',
+      '/./src/images/environmentMap/px.png',
+      '/./src/images/environmentMap/nx.png',
+      '/./src/images/environmentMap/py.png',
+      '/./src/images/environmentMap/ny.png',
+      '/./src/images/environmentMap/pz.png',
+      '/./src/images/environmentMap/nz.png',
     ])
     environmentMap.encoding = THREE.sRGBEncoding
     scene.background = environmentMap
     scene.environment = environmentMap
     // load skill-hat
-    gltfLoader.load('/src/models/001.gltf', (gltf) => {
+    gltfLoader.load('/./src/models/001.gltf', (gltf) => {
       const material = new THREE.MeshStandardMaterial({ color: 0xa3a3a3, metalness: 0.8, roughness: 0, });
       ZibadisHolding.add(gltf.scene);
       ZibadisHolding.scale.set(5.2, 5.2, 5.2);
@@ -222,7 +222,7 @@ scene.add(mainGroup);
       const radius = 1.5;
       for (let i = 0; i < numPlates; i++) {
         const angle = (i / numPlates) * Math.PI * 2;
-        gltfLoader.load('/src/models/001.gltf', (gltf) => {
+        gltfLoader.load('/./src/models/001.gltf', (gltf) => {
           // متریال دلخواه
           const material = new THREE.MeshStandardMaterial({
             color: 0xa3a3a3,
